7036dde5bd278ed20ae55b1b95b1b535188d08c9
Vekisa <vekisa123>
2019-04-17 21:12:47 +0200

    Initial commit

---
+++ src/main/java/com/example/agentski_modul/AgentskiModulApplication.java
@@ -0,0 +1,13 @@
+package com.example.agentski_modul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentskiModulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentskiModulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agentski_modul/AgentskiModulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.agentski_modul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgentskiModulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
